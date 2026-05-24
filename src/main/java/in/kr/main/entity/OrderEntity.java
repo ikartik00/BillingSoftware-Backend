5ffb39d675d503c9d99b6d8f49c006b5ac40a43a
ikartik00 <kartikrathore50600@gmail.com>
2026-05-24 16:56:25 +0530

    add fetch eager orderentity

--- src/main/java/in/kr/main/entity/OrderEntity.java
+++ src/main/java/in/kr/main/entity/OrderEntity.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -48,7 +49,7 @@ public class OrderEntity {
 	@JoinColumn(name = "shop_id", nullable = false)
 	private ShopEntity shop;
 	
-	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true )
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER )
 	private List<OrderItemsEntity> orderItems = new ArrayList<>();
 	
 	@Embedded
